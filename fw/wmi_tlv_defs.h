072634a422054f9ac27b5c1bf78351f12db95111
spuligil <quic_spuligil@quicinc.com>
2023-03-17 06:01:03 -0700

    fw-api: CL 22114305 - update fw common interface files
    Change-Id: I29faa4b558899070e10c2ce30b7481b11ac28da1
    WMI: add MLO_LINK_DISABLE_REQUEST_EVENT msg def
    CRs-Fixed: 2262693

--- fw/wmi_tlv_defs.h
+++ fw/wmi_tlv_defs.h
@@ -1346,6 +1346,19 @@ typedef enum {
     WMITLV_TAG_STRUC_wmi_mlo_vdev_get_link_info_cmd_fixed_param,
     WMITLV_TAG_STRUC_wmi_mlo_vdev_link_info_event_fixed_param,
     WMITLV_TAG_STRUC_wmi_mlo_vdev_link_info,
+    WMITLV_TAG_STRUC_wmi_vdev_set_manual_su_trig_cmd_fixed_param,
+    WMITLV_TAG_STRUC_wmi_vdev_set_manual_mu_trig_cmd_fixed_param,
+    WMITLV_TAG_STRUC_wmi_manual_ul_ofdma_trig_feedback_evt_fixed_param,
+    WMITLV_TAG_STRUC_wmi_mlo_peer_link_control_param,
+    WMITLV_TAG_STRUC_wmi_dma_buf_release_cv_upload_meta_data,
+    WMITLV_TAG_STRUC_wmi_standalone_sounding_cmd_fixed_param,
+    WMITLV_TAG_STRUC_wmi_standalone_sounding_evt_fixed_param,
+    WMITLV_TAG_STRUC_wmi_ctrl_path_blanking_stats_struct,
+    WMITLV_TAG_STRUC_wmi_pdev_set_rf_path_cmd_fixed_param,
+    WMITLV_TAG_STRUC_wmi_manual_ul_ofdma_trig_rx_peer_userinfo,
+    WMITLV_TAG_STRUC_wmi_manual_ul_ofdma_trig_rx_peer_userinfo_evt_fixed_param,
+    WMITLV_TAG_STRUC_wmi_cca_busy_subband_info,
+    WMITLV_TAG_STRUC_wmi_mlo_link_disable_request_event_fixed_param,
 } WMITLV_TAG_ID;
 
 /*
@@ -2169,6 +2182,10 @@ typedef enum {
     OP(WMI_PDEV_SET_TGTR2P_TABLE_EVENTID) \
     OP(WMI_CSA_IE_RECEIVED_EVENTID) \
     OP(WMI_MLO_VDEV_LINK_INFO_EVENTID) \
+    OP(WMI_MANUAL_UL_OFDMA_TRIG_FEEDBACK_EVENTID) \
+    OP(WMI_VDEV_STANDALONE_SOUND_COMPLETE_EVENTID) \
+    OP(WMI_MANUAL_UL_OFDMA_TRIG_RX_PEER_USERINFO_EVENTID) \
+    OP(WMI_MLO_LINK_DISABLE_REQUEST_EVENTID) \
     /* add new EVT_LIST elements above this line */
 
 
@@ -7222,6 +7239,10 @@ WMITLV_CREATE_PARAM_STRUC(WMI_VDEV_STANDALONE_SOUND_COMPLETE_EVENTID);
     WMITLV_ELEM(id,op,buf,len, WMITLV_TAG_ARRAY_STRUC, wmi_manual_ul_ofdma_trig_rx_peer_userinfo, rx_peer_userinfo, WMITLV_SIZE_VAR)
 WMITLV_CREATE_PARAM_STRUC(WMI_MANUAL_UL_OFDMA_TRIG_RX_PEER_USERINFO_EVENTID);
 
+/* MLO_LINK_DISABLE_REQUEST Event */
+#define WMITLV_TABLE_WMI_MLO_LINK_DISABLE_REQUEST_EVENTID(id,op,buf,len) \
+    WMITLV_ELEM(id,op,buf,len, WMITLV_TAG_STRUC_wmi_mlo_link_disable_request_event_fixed_param, wmi_mlo_link_disable_request_event_fixed_param, fixed_param, WMITLV_SIZE_FIX)
+WMITLV_CREATE_PARAM_STRUC(WMI_MLO_LINK_DISABLE_REQUEST_EVENTID);
 
 
 #ifdef __cplusplus
